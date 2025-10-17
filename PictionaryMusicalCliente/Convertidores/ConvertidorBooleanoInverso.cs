403e468c5999b7aa3529fbcdd79f9110699cc9c9
migueleelg0106 <miguelguevara850523@gmail.com>
2025-10-16 20:46:56 -0600

    Agregar infraestructura base de comandos y convertidores

---
+++ PictionaryMusicalCliente/Convertidores/ConvertidorBooleanoInverso.cs
@@ -0,0 +1,34 @@
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PictionaryMusicalCliente.Convertidores
+{
+    /// <summary>
+    /// Invierte valores booleanos permitiendo utilizarlos en bindings sin crear
+    /// propiedades adicionales en la vista modelo.
+    /// </summary>
+    public class ConvertidorBooleanoInverso : IValueConverter
+    {
+        /// <inheritdoc />
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValor)
+            {
+                return !boolValor;
+            }
+
+            return value;
+        }
+
+        /// <inheritdoc />
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValor)
+            {
+                return !boolValor;
+            }
+
+            return value;
+        }
+    }
+}
